623b5d40a446c56385eefefd9aa7b6e1667a614a
Sabrina Hennrich <sabrina@digimedialoop.de>
2024-10-14 10:14:13 +0200

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,11 +7,23 @@ module.exports = [
       contentSecurityPolicy: {
         directives: {
           'img-src': ['*'],
+          'default-src': ["'self'", 'https:'],
+          'script-src': ["'self'", 'https:'],
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'frame-src': ["'self'", 'https:'],
         },
-      }
+      },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://test.digimedialoop.de', 'https://digimedialoop.de', 'http://localhost:3000'], // Füge hier deine Domains hinzu
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
